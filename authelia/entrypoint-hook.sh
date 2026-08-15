a560733966c0a204bf02e53196e36a28b8921540
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-08-15 21:07:06 +0200

    uses username directly

--- authelia/entrypoint-hook.sh
+++ authelia/entrypoint-hook.sh
@@ -13,15 +13,14 @@ if [ ! -f "/secrets/storage_key" ]; then
     head -c 64 /dev/urandom | base64 > /secrets/storage_key
 fi
 
-[ -z "$PUID" ] && PUID="0"
-[ -z "$UID" ] && UID="$PUID"
+CRONTAB_USER="root"
 # shellcheck disable=SC2155 # Exit code is ok to be masked in this case
 export LLDAP_LDAP_BASE_DN="$(echo "$APP_HOST" | sed 's/\./,DC=/g' | sed 's/^/DC=/')"
 
 tmpCronFile="$(mktemp)"
-crontab -u "$(id -nu "${UID}")" -l 2>/dev/null | grep -v '/backup\.sh$' >"$tmpCronFile"
+crontab -u "$CRONTAB_USER" -l 2>/dev/null | grep -v '/backup\.sh$' >"$tmpCronFile"
 echo "${AUTHELIA_BACKUP_SCHEDULE} /backup.sh" >>"$tmpCronFile"
-crontab -u "$(id -nu "${UID}")" "$tmpCronFile" || exit 1
+crontab -u "$CRONTAB_USER" "$tmpCronFile" || exit 1
 rm "$tmpCronFile"
 
 crond -b -l 2
--- lldap/entrypoint-hook.sh
+++ lldap/entrypoint-hook.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-[ -z "$UID" ] && UID="0"
+CRONTAB_USER="root"
 # shellcheck disable=SC2155 # Exit code is ok to be masked in this case
 export LLDAP_LDAP_BASE_DN="$(echo "$APP_HOST" | sed 's/\./,dc=/g' | sed 's/^/dc=/')"
 
 tmpCronFile="$(mktemp)"
-crontab -u "$(id -nu "${UID}")" -l 2>/dev/null | grep -v '/backup\.sh$' >"$tmpCronFile"
+crontab -u "$CRONTAB_USER" -l 2>/dev/null | grep -v '/backup\.sh$' >"$tmpCronFile"
 echo "${LLDAP_BACKUP_SCHEDULE} /backup.sh" >>"$tmpCronFile"
-crontab -u "$(id -nu "${UID}")" "$tmpCronFile" || exit 1
+crontab -u "$CRONTAB_USER" "$tmpCronFile" || exit 1
 rm "$tmpCronFile"
 
 crond -b -l 2
