209d028d64cfc76b280c3c7b490beef3d1fba5d1
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-08-15 20:07:06 +0200

    changes logging and error handling

--- lldap/backup.sh
+++ lldap/backup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+ANY_FAILURE=""
 BASE_PATH="/data"
 RETENTION_DAYS="${LLDAP_BACKUP_RETENTION_DAYS:-2}"
 BACKUP_DIR="/backups/$(basename "$BASE_PATH")/$(TZ="UTC" date +"%Y-%m-%dT%H:%M:%SZ")"
 mkdir -p "$BACKUP_DIR"
 
 backupFailed() {
+    ANY_FAILURE="_"
     originFilePath="$1"; shift
     originExitCode="$1"; shift
     echo "Backup of file '$originFilePath' failed with exit code '$originExitCode', continuing to next file..."
@@ -21,14 +23,16 @@ find "$BASE_PATH" -type f | sort | while read -r filePath; do
     mkdir -p "$(dirname "$backupPath")"
     if [ "$fileExtension" == "db" ] || [ "$fileExtension" == "sqlite" ] || [ "$fileExtension" == "sqlite3" ]; then
         echo "Extension is '$fileExtension' => Hot backup of SQLite database"
-        sqlite3 "$filePath" ".backup '$backupPath'" || backupFailed "$filePath" "$?"
+        sqlite3 "$filePath" ".backup '$backupPath'"#
+        exitCode="$?"; [ "$exitCode" -ne "0" ] && backupFailed "$filePath" "$exitCode"
     else
         echo "Extension is '${fileExtension:-(empty)}' => Simple copy operation"
-        cp -a "$filePath" "$backupPath" || backupFailed "$filePath" "$?"
+        cp -a "$filePath" "$backupPath"
+        exitCode="$?"; [ "$exitCode" -ne "0" ] && backupFailed "$filePath" "$exitCode"
     fi
 done
 
 echo "Cleaning up backups older than $RETENTION_DAYS days..."
 find "/backups/$(basename "$BASE_PATH")" -type d -mindepth 1 -maxdepth 1 -mtime +"$RETENTION_DAYS" -exec rm -rf {} +
 
-echo "Backup job completed successfully."
+{ [ -z "$ANY_FAILURE" ] && echo "Backup job completed successfully."; } || { echo "Backup job caused errors." && exit 1; }
