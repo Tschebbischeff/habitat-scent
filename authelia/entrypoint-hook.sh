2444fecdf0c0d8fe99ba32d6b0029299b489d24d
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-08-16 00:02:54 +0200

    starts with authelia as base image and adds static dependencies via builder from alpine

--- authelia/entrypoint-hook.sh
+++ authelia/entrypoint-hook.sh
@@ -17,6 +17,8 @@ CRONTAB_USER="root"
 # shellcheck disable=SC2155 # Exit code is ok to be masked in this case
 export LLDAP_LDAP_BASE_DN="$(echo "$APP_HOST" | sed 's/\./,DC=/g' | sed 's/^/DC=/')"
 
+mkdir -p /var/spool/cron/crontabs
+
 tmpCronFile="$(mktemp)"
 crontab -u "$CRONTAB_USER" -l 2>/dev/null | grep -v '/backup\.sh$' >"$tmpCronFile"
 echo "${AUTHELIA_BACKUP_SCHEDULE} /backup.sh" >>"$tmpCronFile"
@@ -24,14 +26,5 @@ crontab -u "$CRONTAB_USER" "$tmpCronFile" || exit 1
 rm "$tmpCronFile"
 
 crond -b -l 2
-
-env
-echo "--"
-which authelia
-echo "--"
-ls -la /app
-echo "-- DEBUG SLEEP 10..."
-
-sleep 10
 
 exec "$@"
